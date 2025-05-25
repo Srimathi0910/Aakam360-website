af27a77bb3fb8318d9ae678d629ec1b7866d1f08
Srimathi0910 <srimathinagarajan0910@gmail.com>
2025-05-25 22:21:27 +0530

    Adding Files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -174,12 +174,177 @@
 // export default App;
 
 
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { ThemeProvider } from './ThemeContext';
+// import Lottie from "lottie-react";
+// import loadingAnimation from "../src/img/Loading.json";
+// import './styles.css';
+
+// // Components
+// import Header from './Header';
+// import Allone from './Allone';
+// import Our_5i from './Our_5i';
+// import Do from './Do';
+// import Our_mentors from './Our_mentors';
+// import Our_partners from './Our_partners';
+// import Our_usp from './Our_usp';
+// import Our_outcomes from './Our_outcomes';
+// import History from './History';
+// import Events from './Events';
+// import Career from './Career';
+// import Technology from './Technology';
+// import Feedback from './Feedback';
+// import Contact from './Contact';
+// import Footer from './Footer';
+// import Banking from './Banking';
+// import ConsumerGoodsDistribution from './ConsumerGoodsDistribution';
+// import CommunicationMedia from './CommunicationMedia';
+// import Education from './Education';
+// import EnergyResource from './EnergyResource';
+// import HealthCare from './HealthCare';
+// import HighTech from './HighTech';
+// import LifeScience from './LifeScience';
+// import Manufacturing from './Manufacturing';
+// import PublicServices from './PublicServices';
+// import Retail from './Retail';
+// import TravelAndLogistics from './TravelAndLogistics';
+// import Cloud from './Cloud';
+// import BlockChain from './BlockChain';
+// import IoT from './IoT';
+// import Metaverse from './Metaverse';
+// import FutureOfWork from './FutureOfWork';
+// import HealthWellness from './HealthWellness';
+// import Sustainability from './Sustainability';
+// import AIML from './AIML';
+// import Cybersecurity from './Cybersecurity';
+// import PrivacyPolicy from './PrivacyPolicy';
+// import Disclaimer from './Disclaimer';
+// import Our_5i_explain from './Our_5i_explain';
+// import ScrollToTop from './ScrollToTop';
+// import Industry_join from './Industry_join';
+// import Institution_join from './Institution_join';
+// import TrainingAndPartner from './TrainingAndPartner';
+// import StartupsForm from './StartupsForm';
+// import ApplyInternshipForm from './ApplyInternshipForm';
+// import LearnMore from './LearnMore';
+// import BlogPage from './BlogPage';
+// import Services1 from './Services1';
+// import Services2 from './Services2';
+// import Services3 from './Services3';
+// import Services4 from './Services4';
+// import Services5 from './Services5';
+// import JobApplyForm from './JobApplyForm';
+// import Message from './Message';
+// import Message1 from './Message1';
+// import GetAQuote from './GetAQuote';
+// import Infrastructure from './Infrastructure';
+// import InnovationForm from './InnovationForm';
+// import LoginPage from './LoginPage';
+// import SignUp from './SignUp';
+
+// const App = () => {
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setLoading(false);
+//     }, 2000);
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <div className="loader-container">
+//         <Lottie animationData={loadingAnimation} loop={true} className="lottie-animation" />
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <Router>
+//       <ThemeProvider>
+//         {/* <ScrollToTop />  */}
+//         <Header />
+//         <div className="content">
+//           <Routes>
+//             <Route path="/" element={<Allone />} />
+//             <Route path="/our-5i" element={<Our_5i />} />
+//             <Route path="/our-5i-explain" element={<Our_5i_explain />} />
+//             <Route path="/career" element={<Career />} />
+//             <Route path="/technology" element={<Technology />} />
+//             <Route path="/feedback" element={<Feedback />} />
+//             <Route path="/do" element={<Do />} />
+//             <Route path="/our-mentors" element={<Our_mentors />} />
+//             <Route path="/our-partners" element={<Our_partners />} />
+//             <Route path="/our-usp" element={<Our_usp />} />
+//             <Route path="/our-outcomes" element={<Our_outcomes />} />
+//             <Route path="/history" element={<History />} />
+//             <Route path="/events" element={<Events />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/banking" element={<Banking />} />
+//             <Route path="/consumerGoods" element={<ConsumerGoodsDistribution />} />
+//             <Route path="/communication-media" element={<CommunicationMedia />} />
+//             <Route path="/education" element={<Education />} />
+//             <Route path="/energy-resourse" element={<EnergyResource />} />
+//             <Route path="/healthcare" element={<HealthCare />} />
+//             <Route path="/high-tech" element={<HighTech />} />
+//             <Route path="/life-science" element={<LifeScience />} />
+//             <Route path="/manufacturing" element={<Manufacturing />} />
+//             <Route path="/public-services" element={<PublicServices />} />
+//             <Route path="/retail" element={<Retail />} />
+//             <Route path="/travel-logistics" element={<TravelAndLogistics />} />
+//             <Route path="/cloud" element={<Cloud />} />
+//             <Route path="/blockchain" element={<BlockChain />} />
+//             <Route path="/iot" element={<IoT />} />
+//             <Route path="/metaverse" element={<Metaverse />} />
+//             <Route path="/future-of-Work" element={<FutureOfWork />} />
+//             <Route path="/health-wellness" element={<HealthWellness />} />
+//             <Route path="/sustainability" element={<Sustainability />} />
+//             <Route path="/ai-ml" element={<AIML />} />
+//             <Route path="/cybersecurity" element={<Cybersecurity />} />
+//             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+//             <Route path="/disclaimer" element={<Disclaimer />} />
+//             <Route path="/industry-apply-form" element={<Industry_join />} />
+//             <Route path="/institution-apply-form" element={<Institution_join />} />
+//             <Route path="/training-partner-apply-form" element={<TrainingAndPartner />} />
+//             <Route path="/startup-apply-form" element={<StartupsForm />} />
+//             <Route path="/apply-internship-form" element={<ApplyInternshipForm />} />
+//             <Route path="/LearnMore" element={<LearnMore />} />
+//             <Route path="/blog" element={<BlogPage />} />
+//             <Route path="/Business-Consulting-Solutions" element={<Services1 />} />
+//             <Route path="/Technology-Innovation" element={<Services2 />} />
+//             <Route path="/Education-Training" element={<Services3 />} />
+//             <Route path="/Mentorship-Career-Guidance" element={<Services4 />} />
+//             <Route path="/Research-Development" element={<Services5 />} />
+//             <Route path="/job-apply-form" element={<JobApplyForm />} />
+//             <Route path="/submitted" element={<Message />} />
+//             <Route path="/Applied" element={<Message1 />} />
+//             <Route path="/getaquote" element={<GetAQuote />} />
+//             <Route path="/Infrastructure" element={<Infrastructure />} />
+//             <Route path="/InnovationIdeaForm" element={<InnovationForm />} />
+//             <Route path="/LoginPage" element={<LoginPage />} />
+//             <Route path="/SignUp" element={<SignUp />} />
+//           </Routes>
+//         </div>
+//         {/* <Footer /> */}
+//       </ThemeProvider>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './ThemeContext';
-import Lottie from "lottie-react";
+import Lottie from 'lottie-react';
 import loadingAnimation from "../src/img/Loading.json";
-import './styles.css';
+
+// Theme + Role Selector
+import { ThemeProvider } from './ThemeContext';
+import RoleSelector from './RoleSelector';
 
 // Components
 import Header from './Header';
@@ -196,7 +361,6 @@ import Career from './Career';
 import Technology from './Technology';
 import Feedback from './Feedback';
 import Contact from './Contact';
-import Footer from './Footer';
 import Banking from './Banking';
 import ConsumerGoodsDistribution from './ConsumerGoodsDistribution';
 import CommunicationMedia from './CommunicationMedia';
@@ -221,7 +385,6 @@ import Cybersecurity from './Cybersecurity';
 import PrivacyPolicy from './PrivacyPolicy';
 import Disclaimer from './Disclaimer';
 import Our_5i_explain from './Our_5i_explain';
-import ScrollToTop from './ScrollToTop';
 import Industry_join from './Industry_join';
 import Institution_join from './Institution_join';
 import TrainingAndPartner from './TrainingAndPartner';
@@ -245,12 +408,18 @@ import SignUp from './SignUp';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [userRole, setUserRole] = useState(sessionStorage.getItem('userRole'));
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 2000);
   }, []);
+
+  const handleRoleSelection = (role) => {
+    sessionStorage.setItem('userRole', role);
+    setUserRole(role);
+  };
 
   if (loading) {
     return (
@@ -260,10 +429,13 @@ const App = () => {
     );
   }
 
+  if (!userRole) {
+    return <RoleSelector onSelectRole={handleRoleSelection} />;
+  }
+
   return (
     <Router>
       <ThemeProvider>
-        {/* <ScrollToTop />  */}
         <Header />
         <div className="content">
           <Routes>
@@ -326,7 +498,6 @@ const App = () => {
             <Route path="/SignUp" element={<SignUp />} />
           </Routes>
         </div>
-        {/* <Footer /> */}
       </ThemeProvider>
     </Router>
   );
--- my-app/src/RoleSelector.js
+++ my-app/src/RoleSelector.js
@@ -1,39 +1,89 @@
+// // src/RoleSelector.js
+// import React from 'react';
+// import axios from 'axios';
+// import './styles.css';
+// import Aakamlogo from '../src/img/aakam360-mainlogo.png';
+
+// const RoleSelector = ({ onSelectRole }) => {
+//   const handleRoleSelect = async (e, role) => {
+//     e.preventDefault();
+
+//     try {
+//       await axios.post(`http://localhost:5000/api/visitors/increment/${role}`);
+//       sessionStorage.setItem('userRole', role); // ✅ store only for this session
+//       onSelectRole(role);
+//     } catch (err) {
+//       console.error("Error incrementing count", err);
+//     }
+//   };
+
+//   return (
+//     <div className="role-selector-overlay">
+//       <div className="role-selector-box">
+//       <div className="logo-section1">
+//             <img src={Aakamlogo} alt="Aakam Logo" className="logo-img1" />
+//           </div>
+//         <div className="role-container">
+//           {/* Left - Logo */}
+          
+
+//           {/* Right - Form */}
+//           <form className="role-form" style={{ zIndex: "20000000" }}>
+//             <h2>Welcome to Aakam360! <br />Please choose your role:</h2>
+//             <button onClick={(e) => handleRoleSelect(e, 'students')}>Student</button>
+//             <button onClick={(e) => handleRoleSelect(e, 'industry')}>Industry Professional</button>
+//             <button onClick={(e) => handleRoleSelect(e, 'internship')}>Internship Seeker</button>
+//             <button onClick={(e) => handleRoleSelect(e, 'others')}>Other</button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RoleSelector;
 // src/RoleSelector.js
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './styles.css';
 import Aakamlogo from '../src/img/aakam360-mainlogo.png';
 
 const RoleSelector = ({ onSelectRole }) => {
+  const [error, setError] = useState(null);
+
   const handleRoleSelect = async (e, role) => {
     e.preventDefault();
 
     try {
       await axios.post(`http://localhost:5000/api/visitors/increment/${role}`);
-      sessionStorage.setItem('userRole', role); // ✅ store only for this session
+      sessionStorage.setItem('userRole', role); // Store only for this session
       onSelectRole(role);
     } catch (err) {
-      console.error("Error incrementing count", err);
+      console.error("Error incrementing count:", err);
+      setError("There was a problem. Please try again.");
     }
   };
 
   return (
     <div className="role-selector-overlay">
       <div className="role-selector-box">
-      <div className="logo-section1">
-            <img src={Aakamlogo} alt="Aakam Logo" className="logo-img1" />
-          </div>
-        <div className="role-container">
-          {/* Left - Logo */}
-          
+        <div className="logo-section1">
+          <img src={Aakamlogo} alt="Aakam360 Logo" className="logo-img1" />
+        </div>
 
-          {/* Right - Form */}
-          <form className="role-form" style={{ zIndex: "20000000" }}>
-            <h2>Welcome to Aakam360! <br />Please choose your role:</h2>
+        <div className="role-container">
+          <form className="role-form" style={{ zIndex: 20000000 }}>
+            <h2>
+              Welcome to Aakam360! <br />
+              Please choose your role:
+            </h2>
+
             <button onClick={(e) => handleRoleSelect(e, 'students')}>Student</button>
             <button onClick={(e) => handleRoleSelect(e, 'industry')}>Industry Professional</button>
             <button onClick={(e) => handleRoleSelect(e, 'internship')}>Internship Seeker</button>
             <button onClick={(e) => handleRoleSelect(e, 'others')}>Other</button>
+
+            {error && <p className="error-text">{error}</p>}
           </form>
         </div>
       </div>
